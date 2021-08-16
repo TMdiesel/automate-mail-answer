1ea4ea47e5fa93a9bd0366c1a7657da24be0b916
TMdiesel <cerezzodora2262@icloud.com>
2021-08-16 10:31:16 +0000

    style: format code

--- src/mail_utils.py
+++ src/mail_utils.py
@@ -1,33 +1,36 @@
-#%%
-#default package
+# default package
 import imaplib
 import email
 import re
 import os
 import logging
 import datetime
-#my package
+
+# my package
 import envvar
 
 logger = logging.getLogger("Log").getChild("sub")
-#%%
-def get_msg()->'msg':
+
+
+def get_msg() -> "msg":
     """
     IMAP serverからGmailの受信メールを取得
 
     Returns:
         msg
-    """    
+    """
     envvar.env_set()
-    UserName = os.environ.get('UserName','') 
-    PassName = os.environ.get('PassName','') 
-    CompanyEmail = os.environ.get('CompanyEmail','') 
+    UserName = os.environ.get("UserName", "")
+    PassName = os.environ.get("PassName", "")
+    CompanyEmail = os.environ.get("CompanyEmail", "")
 
-    gmail = imaplib.IMAP4_SSL("imap.gmail.com", '993')
+    gmail = imaplib.IMAP4_SSL("imap.gmail.com", "993")
     gmail.login(UserName, PassName)
     gmail.select()
-    yesterday = (datetime.datetime.now()-datetime.timedelta(days=1)).strftime('%d-%b-%Y')
-    search_option = '(FROM "%s" SENTSINCE "%s")'%(CompanyEmail,yesterday)
+    yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime(
+        "%d-%b-%Y"
+    )
+    search_option = '(FROM "%s" SENTSINCE "%s")' % (CompanyEmail, yesterday)
 
     try:
         typ, data = gmail.search(None, search_option)
@@ -36,7 +39,7 @@ def get_msg()->'msg':
         logger.error(e)
 
     for num in data[0].decode().split():
-        typ, data = gmail.fetch(num, '(RFC822)')
+        typ, data = gmail.fetch(num, "(RFC822)")
         msg = email.message_from_string(data[0][1].decode())
 
     gmail.close()
@@ -45,15 +48,15 @@ def get_msg()->'msg':
     return msg
 
 
-def _get_content(msg)->str:
+def _get_content(msg) -> str:
     """
     msgからメール本文を取得する
-    
+
     Args:
-        msg 
+        msg
     Returns:
         str: メール本文
-    """    
+    """
     charset = msg.get_content_charset()
     payload = msg.get_payload(decode=True)
     try:
@@ -66,29 +69,29 @@ def _get_content(msg)->str:
             return ""
     except Exception as e:
         logger.error(e)
-        return payload # デコードできない場合は生データにフォールバック
+        return payload  # デコードできない場合は生データにフォールバック
 
-def content_to_URL(msg)->str:
+
+def content_to_URL(msg) -> str:
     """
     メール本文からURLを取り出す
     Args:
-        msg 
+        msg
     Returns:
         URL
-    """    
-    content=_get_content(msg)
-    content_list=re.split('[\r|\n]', content)
-    URL=[content for content in content_list if content.startswith('http')][0]
+    """
+    content = _get_content(msg)
+    content_list = re.split("[\r|\n]", content)
+    URL = [content for content in content_list if content.startswith("http")][0]
     logger.info(URL)
 
     return URL
-#%%
+
+
 if __name__ == "__main__":
     """
     test
     """
-    msg=get_msg()
-    URL=content_to_URL(msg)
+    msg = get_msg()
+    URL = content_to_URL(msg)
     print(URL)
-
-
--- src/main.py
+++ src/main.py
@@ -1,35 +1,33 @@
-#%%
-#default package
+# default package
 import datetime
-import time
 import logging
-#third package
+
+# third package
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-import schedule
 from selenium.webdriver.common.alert import Alert
 import jpholiday
-#my package
+
+# my package
 import mail_utils as mu
 
-formatter = '%(levelname)s : %(name)s : %(asctime)s : %(message)s'
-logging.basicConfig(filename='./log/logger.log',level=logging.INFO,format=formatter)
 logger = logging.getLogger("Log")
 
-def _isBizDay()->bool:
+
+def _isBizDay() -> bool:
     """
     今日が休日かどうか判定する
     Return:
         休日:True
         営業日:False
     """
-    Date=datetime.datetime.now()
+    Date = datetime.datetime.now()
     if Date.weekday() >= 5 or jpholiday.is_holiday(Date):
         return True
     else:
         return False
 
-def automate_chrome(URL:str,save_ss:bool=True)->None:
+
+def automate_chrome(URL: str, save_ss: bool = True) -> None:
     """
     seleniumでchromeを操作して回答を登録する
 
@@ -37,47 +35,59 @@ def automate_chrome(URL:str,save_ss:bool=True)->None:
         URL (str): 体調確認回答のURL
     """
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    #options.add_argument('--log-level=3')
+    options.add_argument("--headless")
 
-    driver = webdriver.Chrome("./driver/chromedriver.exe",options=options)
+    driver = webdriver.Chrome("./driver/chromedriver.exe", options=options)
     driver.get(URL)
-    
-    try:
-        #answer
-        driver.find_element_by_xpath("//select[@name='inputcmb_1']/option[2]").click()  #Q1:いいえ
-        driver.find_element_by_xpath("//select[@name='inputcmb_2']/option[2]").click()  #Q2:いいえ
-        driver.find_element_by_xpath("//select[@name='inputcmb_4']/option[8]").click()  #Q4:オフィス在籍
-        driver.find_element_by_xpath("//select[@name='inputcmb_5']/option[8]").click()  #Q5:自宅(テレワーク・休み等)
-        if _isBizDay():
-            driver.find_element_by_xpath("//select[@name='inputcmb_3']/option[4]").click()  #Q3:休み
-        else:
-            driver.find_element_by_xpath("//select[@name='inputcmb_3']/option[3]").click()  #Q3:テレワーク
-        if save_ss:
-            now = datetime.datetime.now().strftime('%Y%m%d-%H%M%S') 
-            driver.save_screenshot("./png/answer_%s.png"%(now))
-            logger.info('save screenshot of your answer')
 
-        #register
+    try:
+        # answer
+        # Q1:いいえ
+        driver.find_element_by_xpath("//select[@name='inputcmb_1']/option[2]").click()
+        # Q2:いいえ
+        driver.find_element_by_xpath("//select[@name='inputcmb_2']/option[2]").click()
+        # Q4:オフィス在籍
+        driver.find_element_by_xpath("//select[@name='inputcmb_4']/option[8]").click()
+        # Q5:自宅(テレワーク・休み等)
+        driver.find_element_by_xpath("//select[@name='inputcmb_5']/option[8]").click()
+        # Q3:休み or テレワーク
+        if _isBizDay():
+            driver.find_element_by_xpath(
+                "//select[@name='inputcmb_3']/option[4]"
+            ).click()
+        else:
+            driver.find_element_by_xpath(
+                "//select[@name='inputcmb_3']/option[3]"
+            ).click()
+        if save_ss:
+            now = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+            driver.save_screenshot("./png/answer_%s.png" % (now))
+            logger.info("save screenshot of your answer")
+
+        # register
         driver.find_element_by_xpath("//input[@name='btnReg2']").click()
         Alert(driver).accept()
         if save_ss:
-            now = datetime.datetime.now().strftime('%Y%m%d-%H%M%S') 
-            driver.save_screenshot("./png/register_%s.png"%(now))
-            logger.info('save screenshot of your register')
-        logger.info('register your answer')
+            now = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+            driver.save_screenshot("./png/register_%s.png" % (now))
+            logger.info("save screenshot of your register")
+        logger.info("register your answer")
     except Exception as e:
         logger.error(e)
 
     driver.quit()
 
-def main()->None:
-    msg=mu.get_msg()
-    URL=mu.content_to_URL(msg)
+
+def main() -> None:
+    formatter = "%(levelname)s : %(name)s : %(asctime)s : %(message)s"
+    logging.basicConfig(
+        filename="./log/logger.log", level=logging.INFO, format=formatter
+    )
+
+    msg = mu.get_msg()
+    URL = mu.content_to_URL(msg)
     automate_chrome(URL)
 
-#%%
-if __name__=='__main__':
+
+if __name__ == "__main__":
     main()
-
-
